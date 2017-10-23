fff069a3e1fa7ae1fe312c0ff250790480fda683
stekern <stekern@users.noreply.github.com>
2017-10-23 03:56:38 -0700

    Initial commit

---
+++ src/bag_detector.py
@@ -0,0 +1,262 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 rsa <dev@ekern.me>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+import rospy
+import message_filters
+import image_geometry
+import cv2
+import numpy as np
+from helpmecarry.msg import Bag3D
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image, CameraInfo
+from geometry_msgs.msg import PointStamped
+
+
+# The percentage of cropping
+BOUNDING_BOX_OFFSET = 0.25
+RECTANGLE_RATIO_BOUNDARIES = (0.70, 0.90)
+BAG_HEIGHT_HANDLE_RATIO = 3
+
+# Camera topics
+RGB_TOPIC = '/hsrb/head_rgbd_sensor/rgb/image_rect_color'
+DEPTH_TOPIC = '/hsrb/head_rgbd_sensor/depth_registered/image_rect_raw'
+CAM_INFO_TOPIC = '/hsrb/head_rgbd_sensor/rgb/camera_info'
+
+# Lower and upper thresholds for color thresholding
+COLOR_BOUNDARIES = {
+        'BLUE': ([30, 50, 30], [220, 190, 100]),
+        'RED': ([20, 20, 90], [60, 60, 255]),
+        'YELLOW': ([0, 120, 120], [150, 255, 255]),
+        'GREEN': ([10, 120, 80], [70, 240, 150]),
+        }
+
+
+class BagDetector(object):
+
+    def __init__(self):
+
+        rospy.init_node('bag_detector', anonymous=True)
+        self.bridge = CvBridge()
+
+        rgb_sub = message_filters.Subscriber(RGB_TOPIC, Image)
+        depth_sub = message_filters.Subscriber(DEPTH_TOPIC, Image)
+        self.ts = message_filters.ApproximateTimeSynchronizer([rgb_sub, depth_sub], 10, 1)
+        self.ts.registerCallback(self.callback)
+
+        # The camera model is used to convert image pixels to 3D rays
+        cam_info = rospy.wait_for_message(CAM_INFO_TOPIC, CameraInfo, timeout=None)
+        self.camera_model = image_geometry.PinholeCameraModel()
+        self.camera_model.fromCameraInfo(cam_info)
+
+        # Topic to publish bag location on
+        self.bag_pub = rospy.Publisher('bag_location', Bag3D, queue_size=10)
+
+
+    def callback(self, raw_rgb_image, raw_depth_image):
+        # Convert rgb image to openCV image
+        rgb_image = self.bridge.imgmsg_to_cv2(raw_rgb_image, 'bgr8')
+
+        # Convert depth image to openCV image
+        depth_image = self.bridge.imgmsg_to_cv2(raw_depth_image, 'passthrough')
+
+        # Apply color mask
+        color_filtered = self.apply_color_mask(rgb_image)
+        cv2.imshow('Color filtered', color_filtered)
+        
+        # Apply saturation filter
+        saturation_filtered = self.apply_saturation_filter(color_filtered)
+        cv2.imshow('Saturation filtered', saturation_filtered)
+
+        # Find the contours
+        contours, _ = cv2.findContours(saturation_filtered, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Only keep the 10 largest contours
+        contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
+
+        # Draw detected objects
+        image_draw = self.get_image_with_objects(contours, rgb_image, depth_image)
+
+        # Display the image
+        cv2.imshow('Detected bags', image_draw)
+        cv2.waitKey(3)
+
+    def get_cropped_bounding_box(
+        self,
+        bounding_box,
+        width,
+        height,
+        offset=BOUNDING_BOX_OFFSET
+    ):
+        """
+        Returns the coordinates of a cropped bounding box
+        """
+        xmin, ymin = bounding_box.min(axis=0)
+        xmax, ymax = bounding_box.max(axis=0)
+
+        xmin += int(width * offset)
+        xmax -= int(width * offset)
+        ymin += int(height * offset)
+        ymax -= int(height * offset)
+
+        return ymin, ymax, xmin, xmax
+
+    def apply_color_mask(self, image, boundaries=COLOR_BOUNDARIES, combine_masks=False):
+        if combine_masks:
+            mask = self.get_combined_color_mask(image)
+        else:
+            # Only use a yellow color mask by default
+            lower, upper = boundaries['YELLOW']
+            lower = np.array(lower, dtype = "uint8")
+            upper = np.array(upper, dtype = "uint8")
+ 
+        # Apply color mask
+	mask = cv2.inRange(image, lower, upper)
+	color_filtered = cv2.bitwise_and(image, image, mask = mask)
+
+        # Apply median blur to reduce noise
+        blurred = cv2.medianBlur(color_filtered, 11)
+
+        return blurred
+
+    def apply_saturation_filter(self, image):
+        # Convert image to hsv
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        hue, saturation, value = cv2.split(hsv)
+
+        # Do a threshold on the saturation
+        threshold = cv2.threshold(saturation, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+        # Smooth the image to remove noise
+        blurred = cv2.medianBlur(threshold, 5)
+
+        return blurred
+
+    def publish_bag_information(self, center, point3d):
+        bag = Bag3D()
+
+        bag_handle = PointStamped()
+        bag_handle.header.frame_id = self.camera_model.tfFrame()
+        bag_handle.header.stamp = rospy.Time.now()
+        bag_handle.point.x = point3d[0]
+        bag_handle.point.y = point3d[1]
+        bag_handle.point.z = point3d[2]
+
+        bag.bag_handle = bag_handle
+        bag.center_x, bag.center_y = center
+
+        self.bag_pub.publish(bag)
+
+    def get_image_with_objects(self, contours, rgb_image, depth_image, ratio_threshold=RECTANGLE_RATIO_BOUNDARIES):
+        draw_image = rgb_image.copy()
+        for c in contours:
+            # Compute the center of the contour
+            M = cv2.moments(c)
+            if M['m00'] == 0.0: continue
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+
+            # Skip contours that do not have four corners
+            if self.get_corners(c) != 4: continue
+
+            # Get the rotated rectangle surrounding the contour
+            rect = cv2.minAreaRect(c)
+            center = rect[0]
+            width, height = rect[1]
+            angle = rect[2]
+
+            # Correct the width, height and angle
+            # See https://namkeenman.wordpress.com/2015/12/18/open-cv-determine-angle-of-rotatedrect-minarearect/
+            if width > height:
+                angle += 90
+                height, width = rect[1]
+
+            ratio = float(width)/height
+
+            # Skip contours where width/height-ratio is off
+            if not (ratio_threshold[0] < ratio < ratio_threshold[1]): continue
+
+            bounding_box = cv2.cv.BoxPoints(rect)
+            bounding_box = np.int0(bounding_box)
+            bounding_box_corners = tuple(map(tuple, bounding_box))
+            ymin, ymax, xmin, xmax = self.get_cropped_bounding_box(bounding_box, width, height)
+
+            # Draw the corners of the rotated bounding box
+            for corner in bounding_box_corners:
+                cv2.circle(draw_image, corner, 3, (255, 0, 0), -1)
+
+            # Crop the bounding box and estimate the depth
+            cropped_bounding_box = depth_image[ymin:ymax, xmin:xmax]
+            if cropped_bounding_box.shape[0]:
+                depth = np.nanmean(cropped_bounding_box)
+            else: continue
+
+            # Draw the cropped bounding box
+            cv2.rectangle(draw_image, (xmin, ymin), (xmax, ymax), (0, 0, 255), 2)
+
+            # Draw the center of the contour and the depth of the object
+            cv2.circle(draw_image, (cX, cY), 4, (255, 255, 255), -1)
+            cv2.putText(draw_image, str(depth), (cX - 20, cY - 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+            # Sort the corners by the y-values
+            bounding_box_corners = sorted(bounding_box_corners, key=lambda x: x[1])
+            # Take the average of the two smallest y-values
+            min_avg_y = sum(x[1] for x in bounding_box_corners[:2])/2
+            # Use the ratio between the bag height and the handle position
+            # to calculate y-value of bag handle
+            bag_handle_y = int(min_avg_y - height/BAG_HEIGHT_HANDLE_RATIO)
+            bag_handle = (cX, bag_handle_y)
+
+            point3d = self.convert_2d_point_to_3d_point(bag_handle, depth)
+            cv2.circle(draw_image, bag_handle, 4, (255, 255, 255), -1)
+
+            # A detection has been made. Publish the necessary information
+            self.publish_bag_information((cX, cY), point3d)
+
+        return draw_image
+
+    def convert_2d_point_to_3d_point(self, pixel, depth):
+        # Convert a pixel with depth to a 3d point
+        ray = np.array(self.camera_model.projectPixelTo3dRay(pixel))
+        point = ray * depth
+        return point
+
+    def get_combined_color_mask(self, image, boundaries=COLOR_BOUNDARIES):
+        masks = []
+        for key, value in boundaries.iteritems():
+            lower = np.array(value[0], dtype='uint8')
+            upper = np.array(value[1], dtype='uint8')
+            mask = cv2.inRange(image, lower, upper)
+            masks.append(mask)
+
+        return self._combine_masks(masks)
+
+    def _combine_masks(self, masks):
+        if len(masks) == 2:
+            return cv2.bitwise_or(masks[0], masks[1])
+        return cv2.bitwise_or(masks[0], self._combine_masks(masks[1::]))
+
+    def get_corners(self, c):
+        # Approximate the contour
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+ 
+        # Return number of detected corners
+        return len(approx)
+
+
+if __name__ == '__main__':
+    BagDetector()
+    try:
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
---
+++ src/bag_grabber.py
@@ -0,0 +1,250 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 rsa <dev@ekern.me>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+import tf
+import rospy
+import math
+import hsrb_interface as hsrb
+import numpy as np
+from helpmecarry.msg import Bag3D
+from geometry_msgs.msg import Twist
+
+# Name of the joints
+ARM_LIFT_JOINT = 'arm_lift_joint'
+ARM_FLEX_JOINT = 'arm_flex_joint'
+ARM_ROLL_JOINT = 'arm_roll_joint'
+WRIST_FLEX_JOINT = 'wrist_flex_joint'
+WRIST_ROLL_JOINT = 'wrist_roll_joint'
+HEAD_PAN_JOINT = 'head_pan_joint'
+HEAD_TILT_JOINT = 'head_tilt_joint'
+
+# Topics for the joints
+BAG_LOCATION_TOPIC = '/bag_location'
+VELOCITY_TOPIC = '/hsrb/command_velocity'
+
+# Constants used for centering the bag in the middle of the camera
+IMAGE_WIDTH = 640
+CENTERING_THRESHOLD = 30
+LINEAR_VELOCITY = 0.04
+
+# Constant for activating different phases of the task
+DEPTH_THRESHOLDS = (0.8, 1.2)
+
+# How many samples to do of bag position
+# before initiating arm movement
+NUM_SAMPLES = 5
+
+# This constant is multiplied by the current depth 
+# to control the number of seconds the robot can 
+# move towards the bag at a time
+TIMEOUT_MULTIPLIER = 2.0
+
+
+
+# 
+MAX_ARM_LIFT = 0.69
+
+# Neutral position
+NEUTRAL_ARM_FLEX_ANGLE = math.radians(-90)
+NEUTRAL_WRIST_ROLL_ANGLE = math.radians(90)
+NEUTRAL_WRIST_FLEX_ANGLE = math.radians(-75) # Slightly inclined wrist
+
+# Grab position
+GRAB_WRIST_FLEX_ANGLE = math.radians(12) # Wrist inclination after claw is inside bag handle
+GRAB_ARM_LIFT_INCREASE = 0.1 # Meters to lift hand after claw is inside bag handle
+GRAB_FORWARD_VELOCITY = 0.2 # Speed used to move horizontally for placing bag in center
+GRAB_BACKWARDS_VELOCITY = -0.1 # Speed used to back up if robot is too close to bag
+
+# Angles for the gripper joint
+GRIPPER_CLOSED = 0.0
+GRIPPER_OPENED = 1.2
+
+# Misc. delays
+SLEEP_DELAY = 2.0 # Number of seconds to wait between control commands that might interfere 
+TF_DELAY = 4.0 # Number of seconds to wait for transformation between two frames
+
+# Frames 
+HAND_FRAME = 'hand_palm_link'
+MAP_FRAME = 'map'
+
+
+class BagGrabber(object):
+    def __init__(self):
+        # Set up the robot
+        self.robot = hsrb.Robot()
+        self.whole_body = self.robot.get('whole_body')
+        self.gripper = self.robot.get('gripper', self.robot.Items.END_EFFECTOR)
+        self.omni_base = self.robot.get('omni_base')
+
+        self.start_position = {
+            ARM_LIFT_JOINT: 0.0,
+            ARM_FLEX_JOINT: 0.0,
+            ARM_ROLL_JOINT: 0.0,
+            WRIST_FLEX_JOINT: NEUTRAL_WRIST_FLEX_ANGLE,
+            WRIST_ROLL_JOINT: NEUTRAL_WRIST_ROLL_ANGLE,
+            HEAD_PAN_JOINT: 0.0,
+            HEAD_TILT_JOINT: 0.0,
+        }
+
+        self.neutral_position = {
+            HEAD_PAN_JOINT: 0.0,
+            HEAD_TILT_JOINT: 0.0,
+        }
+
+        rospy.loginfo('Moving robot to start position')
+        self.whole_body.move_to_joint_positions(self.start_position)
+        self.gripper.command(GRIPPER_OPENED)
+
+        self.samples = []
+
+        # Set flag for first part of movement
+        self.in_position = False
+
+        # Set flag for second and final part of movement
+        self.finished = False
+
+        # Setup TF listener
+        self.listener = tf.TransformListener()
+
+        # Subscribe to bag location topic
+        self.bag_location_sub = rospy.Subscriber(BAG_LOCATION_TOPIC, Bag3D, self.callback, queue_size=1)
+
+        # Velocity publisher is used for simple sideways movement
+        self.velocity_pub = rospy.Publisher(VELOCITY_TOPIC, Twist, queue_size=10)
+
+
+    def is_target_in_center(self, point2d):
+        center = IMAGE_WIDTH/2
+        x, y = point2d
+        if (x > center + CENTERING_THRESHOLD or x < center - CENTERING_THRESHOLD):
+            return False
+        return True
+
+    def center_target(self, point2d, depth):
+        center = IMAGE_WIDTH/2
+        x, y = point2d
+        twist = Twist()
+        # Velocity is multiplied by depth to move slower when close to bag
+        if x > center + CENTERING_THRESHOLD:
+            twist.linear.y = -LINEAR_VELOCITY* depth
+        elif x < center - CENTERING_THRESHOLD:
+            twist.linear.y = LINEAR_VELOCITY* depth
+        self.velocity_pub.publish(twist)
+
+    def transform_point(self, source, target, point):
+        self.listener.waitForTransform(source, target, rospy.Time.now(), rospy.Duration(TF_DELAY))
+        while not rospy.is_shutdown():
+            try:
+                self.listener.waitForTransform(source, target, rospy.Time.now(), rospy.Duration(TF_DELAY))
+                trans = self.listener.transformPoint(target, point)
+                return trans
+            except:
+                continue
+
+    def get_updated_arm_lift(self, change):
+        curr_arm_lift = self.whole_body.joint_state.position[1]
+        new_arm_lift = curr_arm_lift + change
+        if new_arm_lift > MAX_ARM_LIFT:
+            new_arm_lift = MAX_ARM_LIFT
+        elif new_arm_lift < 0:
+            new_arm_lift = 0
+        return new_arm_lift
+
+    def callback(self, bag):
+        bag_handle = bag.bag_handle
+        center = (bag.center_x, bag.center_y)
+        cam_frame = bag_handle.header.frame_id
+        depth = bag_handle.point.z
+
+        if self.finished:
+            self.robot.close()
+        elif not self.is_target_in_center(center) and not self.finished:
+            rospy.loginfo('Centering bag along y-axis')
+            self.center_target(center, depth)
+        elif not self.finished:
+            if depth > DEPTH_THRESHOLDS[1]:
+                trans = self.transform_point(cam_frame, MAP_FRAME, bag_handle) 
+                x = trans.point.x
+                y = trans.point.y
+
+                # Get the current yaw of the robot
+                yaw = self.omni_base.pose[2]
+
+                # Set the timeout for the movement
+                timeout = TIMEOUT_MULTIPLIER * depth
+
+                rospy.loginfo('Moving the robot towards x: %.4f, y: %.4f for %.4f seconds' % (x, y, timeout))
+
+                # Move straight towards the object
+                try:
+                    self.omni_base.go(x, y, yaw, timeout=timeout)
+                except hsrb.exceptions.MobileBaseError:
+                    rospy.logerr('Movement towards bag timed out')
+                    pass
+
+                # Reset head position to get more accurate readings
+                rospy.sleep(SLEEP_DELAY)
+                self.whole_body.move_to_joint_positions(self.neutral_position)
+
+            elif depth < DEPTH_THRESHOLDS[0]:
+                twist = Twist()
+                twist.linear.x = GRAB_BACKWARDS_VELOCITY
+                self.velocity_pub.publish(twist)
+                rospy.loginfo('Robot is too close to bag. Backing up')
+                #rospy.sleep(SLEEP_DELAY)
+
+            elif DEPTH_THRESHOLDS[0] < depth < DEPTH_THRESHOLDS[1]:
+                if not self.in_position:
+                    rospy.loginfo('Setting robot to neutral position')
+                    rospy.sleep(SLEEP_DELAY)
+                    self.whole_body.move_to_joint_positions(self.start_position)
+                    self.in_position = True
+
+                if self.in_position:
+                    trans = self.transform_point(cam_frame, HAND_FRAME, bag_handle)
+                    # Use 
+                    self.samples.append((trans.point.x, trans.point.y, trans.point.z))
+                    if len(self.samples) >= NUM_SAMPLES:
+                        rospy.loginfo('Moving arm to the bag handle')
+                        try:
+                            x = np.mean([s[0] for s in self.samples])
+                            y = np.mean([s[1] for s in self.samples])
+                            z = np.mean([s[2] for s in self.samples])
+
+                            self.whole_body.move_end_effector_pose(hsrb.geometry.pose(x=x, y=y, z=z), HAND_FRAME)
+
+                            rospy.loginfo('Picking up the bag handle')
+                            twist = Twist()
+                            twist.linear.x = GRAB_FORWARD_VELOCITY
+                            self.velocity_pub.publish(twist)
+
+                            # A delay is added here to avoid interference between 
+                            # the previous and next control command
+                            rospy.sleep(SLEEP_DELAY)
+
+                            arm_lift = self.get_updated_arm_lift(GRAB_ARM_LIFT_INCREASE)
+                            self.whole_body.move_to_joint_positions({
+                                ARM_LIFT_JOINT: arm_lift,
+                                WRIST_FLEX_JOINT: GRAB_WRIST_FLEX_ANGLE
+                                })
+
+                            self.finished = True
+                        except hsrb.exceptions.MotionPlanningError:
+                            rospy.logerr('Failed to plan arm movement')
+
+
+if __name__ == '__main__':
+    BagGrabber()
+    try:
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
